57f655f64606f04c64fb221a8bb2e000335893cc
andreasmarcel <andreasmarcel@gmail.com>
2007-06-28 19:56:50 +0000

    -Rewritten code to allow easier addition of functionality. -File sending functionality moved to sendfile.c, most protocol-specific code put in Casio9860.c. -Added support for commandline switches. -Added -t option (throttle) for transfer speed control. -Extended Casio9860.c to do simple tasks like connection verification and returning capacity of flash memory. -Makefile also allows cleaning of object files and temporary files. -Updated licence header for all project files.

--- xfer9860/trunk/usbio.c
+++ xfer9860/trunk/usbio.c
@@ -1,24 +1,23 @@
-/***************************************************************************
- *   xfer9860 - Transfer files between a Casio fx-9860G and computer	   *
- *									   *
- *   Copyright (C) 2007							   *
- *	Andreas Bertheussen <andreasmarcel@gmail.com>			   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+/*******************************************************************************
+	xfer9860 - fx-9860G (SD) communication utility
+	Copyright (C) 2007
+		Andreas Bertheussen <andreasmarcel@gmail.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+	MA  02110-1301, USA.
+*******************************************************************************/
 
 #include "usbio.h"
 
@@ -32,10 +31,10 @@ void debug(int input, char* array, int len){
 
 	if (input) { fprintf(stderr, "<< "); }
 	else { fprintf(stderr, ">> "); }
-	
+
 	for (i = 0 ; i < len ; i++){
 		temp = (unsigned char) array[i];
-		
+
 		if (i % LEN_LINE == 0 && i != 0){
 			fprintf(stderr, "\t");
 			for (j = line; j < line + LEN_LINE; j++){
@@ -49,21 +48,21 @@ void debug(int input, char* array, int len){
 			if (input) { fprintf(stderr, "<< "); }
 			else { fprintf(stderr, ">> "); }
 		}
-		
+
 		fprintf(stderr,"%02X ", temp);
 	}
 
-	if (i % LEN_LINE != 0)	
+	if (i % LEN_LINE != 0)
 		for (j = 0 ; j < (int)(LEN_LINE-(i % LEN_LINE)); j++)
 			fprintf(stderr,"   ");
-	
+
 	fprintf(stderr, "\t");
 	for (j = line; j < len; j++){
 		temp = (short unsigned int) array[j];
 		if (temp > 31)
 			fprintf(stderr, "%c", temp);
 		else
-			fprintf(stderr, ".");			
+			fprintf(stderr, ".");
 	}
 
 	fprintf(stderr,"\n\n");
@@ -73,7 +72,7 @@ void debug(int input, char* array, int len){
 int ReadUSB(struct usb_dev_handle *usb_handle, char *buffer, int length) {
 	int ret = 0;
 	ret = usb_bulk_read(usb_handle, 0x82, buffer, length, USB_READ_TIMEOUT);
-	if (ret < 0) { printf("Could not read, ERROR: %i", ret); }
+	if (ret < 0) { printf("ERR: ReadUSB(): Could not read: %i\n", ret); }
 	debug(1, buffer, ret);
 	return ret;
 }
@@ -81,7 +80,7 @@ int ReadUSB(struct usb_dev_handle *usb_handle, char *buffer, int length) {
 int WriteUSB(struct usb_dev_handle *usb_handle, char *buffer, int length) {
 	int ret = 0;
 	ret = usb_bulk_write(usb_handle, 0x1, buffer, length, USB_WRITE_TIMEOUT);
-	if (ret < 0) { printf("Could not write, ERROR: %i", ret); }
+	if (ret < 0) { printf("ERR: WriteUSB: Could not write: %i\n", ret); }
 	debug(0, buffer, ret);
 	return ret;
 }
