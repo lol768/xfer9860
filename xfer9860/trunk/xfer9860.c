9cee0276588dbb5c7f8e560c5fa6cb48ec455f17
andreasmarcel <andreasmarcel@bcd0202f-8d2d-0410-9da9-a239c3f9f1bf>
2007-07-06 17:08:43 +0000

    * xfer9860.c: added an -a switch (license info) and cleaned up unneeded code.

--- xfer9860/trunk/xfer9860.c
+++ xfer9860/trunk/xfer9860.c
@@ -25,49 +25,50 @@
 #include "uploadfile.h"
 #include "getinfo.h"
 
-const char* _version_ = "0.2";
+const char* _version_ = "SVN";
 
 void displayHelp();
+void displayAbout();
 
 int main(int argc, char *argv[]) {
 	int opt;
+
+	/* parameters */
 	char *sourceFileName;
 	int throttleSetting = 0;
-	int sendFileFlag = 0;
-	int getInfoFlag = 0;
-	int throttleAble = 0;
-	printf("--- xfer9860 v%s Copyright (C) 2007 Andreas Bertheussen and Manuel Naranjo.\n", _version_);
 
-	while ((opt = getopt(argc, argv, "t:hu:i")) != -1) {
+	/* operations */
+	int uploadFileFlag = 0;
+	int getInfoFlag = 0;
+
+	printf("--- xfer9860 %s  Copyright (C) 2007 Andreas Bertheussen and Manuel Naranjo.\n", _version_);
+
+	while ((opt = getopt(argc, argv, "t:u:iha")) != -1) {
 		switch(opt) {
 			case 't':
 				throttleSetting = atoi(optarg);
 				break;
-			case 'h':
-				break;
+			case 'h': break;
 			case 'u':
 				sourceFileName = optarg;
-				sendFileFlag = 1;
-				throttleAble = 1;
+				uploadFileFlag = 1;
 				break;
 			case 'i':
-				getInfoFlag = 1;
-				break;
+				getInfo(throttleSetting);
+				return 0;
+			case 'a':
+				displayAbout();
+				return 0;
 			default:
 				return 0;
 		}
 	}
 
-	if (getInfoFlag) {
-		getInfo(throttleSetting);
-		return 0;
-	}
-
-	if (sendFileFlag) {
+	if (uploadFileFlag) {
 		if (optind >= argc)
 			printf("You must specify a destination file name.\n");
 		else
-			sendFile(sourceFileName, argv[optind], throttleSetting);
+			uploadFile(sourceFileName, argv[optind], throttleSetting);
 		return 0;
 	}
 
@@ -78,15 +79,29 @@ int main(int argc, char *argv[]) {
 void displayHelp() {
 	printf(	"--- a Casio fx-9860G (SD) communication utility.\n"
 		"Usage: xfer9860 <action> destname [-t throttle]\n"
-		"Actions:\n"
+		"Calculator actions:\n"
 		" -u srcname\tUpload file `srcname' from PC to `destname' on device.\n"
-		//" -d srcname\tDownload file `srcname' from device to `destname' on PC.\n"
-		" -i\t\tShow information about the connected calculator.\n"
+		//" -d srcname\tDownload file `srcname' from device to `destname' on PC.\n" // NOT IMPLEMENTED YET
+		" -i\t\tShows information about the connected calculator.\n"
 		"\nParameters:\n"
 		" -t value\tThrottle setting. The value specifies the delay in ms between\n"
 	      	"\t\tpackets. Default is 0. Try increasing this in case of problems.\n"
-	      	"");
+		"\nOther:\n"
+		" -h\t\tDisplay this help message.\n"
+	      	" -a\t\tDisplay info about xfer9860 and its licensing.\n");
 	printf("\n");
+}
 
-
+void displayAbout() {
+	printf(	"--- a Casio fx-9860G (SD) communication utility.\n"
+		"This program is free software; you can redistribute it and/or\n"
+		"modify it under the terms of the GNU General Public License\n"
+		"as published by the Free Software Foundation; either version 2\n"
+		"of the License, or (at your option) any later version.\n\n"
+		"This program is distributed in the hope that it will be useful,\n"
+		"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+		"GNU General Public License for more details.\n\n"
+		"You should have received a copy of the GNU General Public License\n"
+		"along with this program. If not, see <http://www.gnu.org/licenses/>.\n");
 }
